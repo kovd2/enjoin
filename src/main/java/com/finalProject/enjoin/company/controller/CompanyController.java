ce04e8aa45565f2c63c1538b53ce26d7353f849a
syong9233 <syong9233@naver.com>
2018-11-01 21:17:05 +0900

    ㅜㅜ

--- src/main/java/com/finalProject/enjoin/company/controller/CompanyController.java
+++ src/main/java/com/finalProject/enjoin/company/controller/CompanyController.java
@@ -1,7 +1,7 @@
 package com.finalProject.enjoin.company.controller;
 
 import java.io.File;
-import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,41 +27,93 @@ public class CompanyController {
 
 
 	//제휴시설 등록폼 보여주는 메소드
-	@RequestMapping("companyInsertForm.me")
+	@RequestMapping("companyInsertForm.gs")
 	public String showCompanyInsertForm(){
 		return "company/companyInsert";
 	}
 
 	//나의 제휴시설을 보여주는 메소드
-	@RequestMapping("companylist.me")
+	@RequestMapping("companylist.gs")
 	public String showCompany(){
 		return "company/companyView";
 	}
 
 	//나의 시설 이용내역을 보여주는 메소드
-	@RequestMapping("useHistory.me")
+	@RequestMapping("useHistory.gs")
 	public String showUseHistory() {
 		return "company/useHistory";
 	}
 
 	//입장확인 폼을 보여주는 메소드
-	@RequestMapping("enterConfirm.me")
+	@RequestMapping("enterConfirm.gs")
 	public String showEnterConfirm() {
 		return "company/enterConfirm";
 	}
 
 
-	@RequestMapping("facilityInsert.me")
-	public String companyInsert(Model model, Company c, HttpServletRequest request, 
+	@RequestMapping("facilityInsert.gs")
+	public String companyInsert(Model model, /*Company c,*/ HttpServletRequest request, 
 									@RequestParam(name="gs_file", required=false) MultipartFile photo) {
 		System.out.println("photo = " + photo);
-		System.out.println("Company = " + c);
+		
+		//객체 꺼내기
+		/*int facilityNo = Integer.parseInt(request.getParameter("facilityNo"));
+		int facilityCopNo = Integer.parseInt(request.getParameter("facilityCopNo"));
+		String facilityRequestDate2 = request.getParameter("facilityRequestDate");
+		String facilityStartDate2 = request.getParameter("facilityStartDate");
+		String facilityEndDate2 = request.getParameter("facilityEndDate");
+		String facilityStatus = request.getParameter("facilityStatus");*/
+				
+		String facilityName = request.getParameter("facilityName");
+		String facilityArea = request.getParameter("facilityArea");
+		String facilitySection = request.getParameter("facilitySection");
+		String facilityAddress = request.getParameter("facilityAddress");
+		String facilityEvent = request.getParameter("facilityEvent");
+		String facilityManagerName = request.getParameter("facilityManagerName");
+		String facilityManagerPhone = request.getParameter("facilityManagerPhone");
+		String mon_fri_time = request.getParameter("mon_fri_time");
+		String sat_time = request.getParameter("sat_time");
+		String sun_time = request.getParameter("sun_time");
+		String passCount = request.getParameter("passCount");
+		String timeForCall = request.getParameter("timeForCall");
+		
+		//date사용
+		/*java.sql.Date facilityRequestDate = java.sql.Date.valueOf(facilityRequestDate2);
+		java.sql.Date facilityStartDate = java.sql.Date.valueOf(facilityStartDate2);
+		java.sql.Date facilityEndDate = java.sql.Date.valueOf(facilityEndDate2);*/
+		
+		//Company 객체 생성
+		Company c = new Company();
+		
+		//객체에 값 담기
+		/*c.setFacilityNo(facilityNo);
+		c.setFacilityCopNo(facilityCopNo);
+		c.setFacilityRequestDate(facilityRequestDate);
+		c.setFacilityStartDate(facilityStartDate);
+		c.setFacilityEndDate(facilityEndDate);
+		c.setFacilityStatus(facilityStatus);*/
+		
+		c.setFacilityName(facilityName);
+		c.setFacilityArea(facilityArea);
+		c.setFacilitySection(facilitySection);
+		c.setFacilityAddress(facilityAddress);
+		c.setFacilityEvent(facilityEvent);
+		c.setFacilityManagerName(facilityManagerName);
+		c.setFacilityManagerPhone(facilityManagerPhone);
+		c.setMon_fri_time(mon_fri_time);
+		c.setSat_time(sat_time);
+		c.setSun_time(sun_time);
+		c.setPassCount(passCount);
+		c.setTimeForCall(timeForCall);
+		
+		System.out.println("Company : " + c);
+		
 		
 		//사진 저장할 경로 지정
 		String root = request.getSession().getServletContext().getRealPath("resources");
 		
 		//파일의 저장 경로는 root 하위의 uploadfiles이다.
-		String filePath = root + "\\uploadFiles";
+		String filePath = root + "\\uploadFiles\\facility";
 		
 		System.out.println("filePath = " + filePath);
 		
@@ -81,9 +133,10 @@ public class CompanyController {
 		
 		} catch (Exception e) {
 			//실패시 파일 삭제
-			
+			System.out.println("dao company : " + c);
 			new File(filePath + "\\" + changeName + ext).delete();
 			model.addAttribute("msg", "제휴시설신청실패");
+			
 			
 			return "common/errorPage";
 			
