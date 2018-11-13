af59d891ac10e9d48b15102a4e2f10d56cbf30ad
sdw <sdw@shin>
2018-11-13 15:35:54 +0900

    관리자진행중

--- src/main/java/com/finalProject/enjoin/crew/model/vo/Crew.java
+++ src/main/java/com/finalProject/enjoin/crew/model/vo/Crew.java
@@ -17,6 +17,7 @@ public class Crew implements java.io.Serializable{
 	private String board_title;	 //글제목
 	private String board_Content;	//글내용
 	private String user_Name;		//유저네임
+	private String user_Id;			//유저 아이디
 
 	
 	
@@ -26,7 +27,7 @@ public class Crew implements java.io.Serializable{
 
 	public Crew(int crew_Id, String crew_Name, int crew_Max, int user_No, Date request_Date, Date start_Date,
 			Date end_Date, String crew_Status, int category_No, String category_Name, String crew_Address,
-			String board_title, String board_Content, String user_Name) {
+			String board_title, String board_Content, String user_Name, String user_Id) {
 		super();
 		this.crew_Id = crew_Id;
 		this.crew_Name = crew_Name;
@@ -42,6 +43,7 @@ public class Crew implements java.io.Serializable{
 		this.board_title = board_title;
 		this.board_Content = board_Content;
 		this.user_Name = user_Name;
+		this.user_Id = user_Id;
 	}
 
 
@@ -130,6 +132,12 @@ public class Crew implements java.io.Serializable{
 
 
 
+	public String getUser_Id() {
+		return user_Id;
+	}
+
+
+
 	public void setCrew_Id(int crew_Id) {
 		this.crew_Id = crew_Id;
 	}
@@ -214,17 +222,24 @@ public class Crew implements java.io.Serializable{
 
 
 
+	public void setUser_Id(String user_Id) {
+		this.user_Id = user_Id;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Crew [crew_Id=" + crew_Id + ", crew_Name=" + crew_Name + ", crew_Max=" + crew_Max + ", user_No="
 				+ user_No + ", request_Date=" + request_Date + ", start_Date=" + start_Date + ", end_Date=" + end_Date
 				+ ", crew_Status=" + crew_Status + ", category_No=" + category_No + ", category_Name=" + category_Name
 				+ ", crew_Address=" + crew_Address + ", board_title=" + board_title + ", board_Content=" + board_Content
-				+ ", user_Name=" + user_Name + "]";
+				+ ", user_Name=" + user_Name + ", user_Id=" + user_Id + "]";
 	}
 
 
 
+	
 	
 
 
